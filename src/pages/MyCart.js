ae289255872b7ba92c69d9c7449c6a6d1da07181
Lassya aitipamula <161203075+aitipamulalassya@users.noreply.github.com>
2025-08-02 16:49:15 +0530

    Update MyCart.js

--- src/pages/MyCart.js
+++ src/pages/MyCart.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import { useCart } from '../context/CartContext'; // Adjust path based on your project
+import { FaShoppingCart } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 const CartPage = () => {
   const { cartItems, updateQuantity, removeFromCart } = useCart();
 
